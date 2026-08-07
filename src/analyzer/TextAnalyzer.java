6db2dc76970dded0e261b0de5c2916c253f68543
Krishna Tiwari <kt345575@gmail.com>
2026-08-07 17:46:12 +0530

    feat: implement core text analysis metrics

--- src/analyzer/TextAnalyzer.java
+++ src/analyzer/TextAnalyzer.java
@@ -1,32 +1,45 @@
 package analyzer;
 import model.AnalysisResult;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TextAnalyzer {
     
     public AnalysisResult analyze(String text) {
         AnalysisResult result = new AnalysisResult();
-        result.setCharacterCount(analyzeCharCount(text));
-        result.setWordCount(analyzeWordCount(text));
-        result.setLineCount(analyzeLineCount(text));
+        result.setCharacterCount(countCharacters(text));
+        result.setWordCount(countWords(text));
+        result.setLineCount(countLines(text));
+        result.setCharacterFrequency(countCharacterFrequency(text));
         return result;
     }
 
     //=====helpers======
 
-    private int analyzeCharCount(String text){
+    private int countCharacters(String text){
         if(text.isBlank()) return 0;
         return text.length();
     }
-    private int analyzeWordCount(String text){
+    private int countWords(String text){
         if(text.isBlank()) return 0;
         return (text.split("\\s+").length);
     }
-    private int analyzeLineCount(String text){
+    private int countLines(String text){
         if(text.isBlank()) return 0;
         int lineCount = 1;
         for(int i = 0 ; i < text.length() ; i++){
             if(text.charAt(i) == '\n') lineCount++;
         }
         return lineCount;
+    }
+    private Map<Character, Integer> countCharacterFrequency(String text){
+        if(text.isBlank()) return new HashMap<>();
+        text = text.toLowerCase();
+        Map<Character,Integer> frequency = new HashMap<>();
+        for (char ch : text.toCharArray()) {
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+        }
+        return frequency;
     }
 }
