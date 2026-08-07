0f45c2307fc574a755084cae925257acb0394ef7
Krishna Tiwari <kt345575@gmail.com>
2026-08-07 17:33:50 +0530

    feat: add basic text statistics

--- src/analyzer/TextAnalyzer.java
+++ src/analyzer/TextAnalyzer.java
@@ -5,7 +5,28 @@ public class TextAnalyzer {
     
     public AnalysisResult analyze(String text) {
         AnalysisResult result = new AnalysisResult();
-        result.setCharacterCount(text.length());
+        result.setCharacterCount(analyzeCharCount(text));
+        result.setWordCount(analyzeWordCount(text));
+        result.setLineCount(analyzeLineCount(text));
         return result;
+    }
+
+    //=====helpers======
+
+    private int analyzeCharCount(String text){
+        if(text.isBlank()) return 0;
+        return text.length();
+    }
+    private int analyzeWordCount(String text){
+        if(text.isBlank()) return 0;
+        return (text.split("\\s+").length);
+    }
+    private int analyzeLineCount(String text){
+        if(text.isBlank()) return 0;
+        int lineCount = 1;
+        for(int i = 0 ; i < text.length() ; i++){
+            if(text.charAt(i) == '\n') lineCount++;
+        }
+        return lineCount;
     }
 }
